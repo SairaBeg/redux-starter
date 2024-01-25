4a08adbeeb940a4d8ff7b31d163f5b014d892fed
SairaBeg <sairabeg.dev@gmail.com>
2024-01-25 18:30:08 -0500

    lodash added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-function sayHello() {
-  return "hello lowercase through a variable";
-}
-let fn = sayHello();
+import { compose, pipe } from "lodash/fp";
 
-function fnLogger(fctn) {
-  console.log(fctn);
-}
-fnLogger(fn);
+// function sayHello() {
+//   return "hello lowercase through a variable";
+// }
+// let fn = sayHello();
+
+// function fnLogger(fctn) {
+//   console.log(fctn);
+// }
+// fnLogger(fn);
+
+// let numbers = [1, 2, 3];
+
+// console.log(numbers.map((number) => number * 2));
+const input = "  JSbenis  ";
+const trim = (str) => str.trim();
+const wrapInDiv = (str) => `<div>${str}</div>`;
+const toLowerCase = (str) => str.toLowerCase();
+
+const transform = pipe(trim, toLowerCase, wrapInDiv);
+const result = transform(input);
+
+// const result = wrapInDiv(toLowerCase(trim(input)));
+console.log(result);
