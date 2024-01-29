b8d01b32d1b868c38d07f8bd5cd7773627f644e2
SairaBeg <sairabeg.dev@gmail.com>
2024-01-28 20:05:27 -0500

    Switch statement reducer added

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,6 @@
 let lastId = 0;
 
-function reducer(state, action) {
+function reducer(state = [], action) {
   if (action.type === "bugAdded") {
     return [
       ...state,
@@ -12,5 +12,26 @@ function reducer(state, action) {
     ];
   } else if (action.type === "bugRemoved") {
     return state.filter((bug) => bug.id !== action.payload.id);
+  } else {
+    return state;
+  }
+}
+
+function reducer(state = [], action) {
+  switch (action.type) {
+    case "bugAdded":
+      return [
+        ...state,
+        {
+          id: lastId++,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+
+    case "bugRemoved":
+      return state.filter((bug) => bug.id !== action.payload.id);
+    default:
+      return state;
   }
 }
